5a1c316a4086e67abff72bc25b9c88e72f2b3af4
adgai19 <gaitondeaditya@gmail.com>
2020-04-13 18:12:27 +0530

    khbf

--- Mackup/.config/fish/config.fish
+++ Mackup/.config/fish/config.fish
@@ -1,4 +1,4 @@
-alias cat="bat"
+alias cat="batcat"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 alias ls="exa"
 alias l="exa -lahF"
@@ -19,12 +19,15 @@ alias gcma="git commit -a -m"
 alias gs="git status"
 alias gp="git push -u origin"
 alias ga="git add -A"
-begin
-    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
-    if test -e $AUTOJUMP_PATH
-        source $AUTOJUMP_PATH
-    end
-end
+#begin
+#    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+#    if test -e $AUTOJUMP_PATH
+#        source $AUTOJUMP_PATH
+#    end
+#end
 
-source /usr/share/autojump/autojump.fish
+#source /usr/share/autojump/autojump.fish
 set PATH /root/.cargo/bin /home/adgai/.cargo/bin /home/adgai/.local/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /snap/bin
+#set fish_function_path $fish_function_path " /home/adgai/.local/lib/python3.8/site-packages/powerline/bindings/fish"
+#powerline-setup
+export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
