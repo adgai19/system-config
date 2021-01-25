8783e137aeee113ecc71031cf4a8a58d9a277437
adgai19 <gaitondeaditya@gmail.com>
2021-01-25 15:09:57 +0530

    minor config changes

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,30 +1,63 @@
-# Set up the default, mnemonic key bindings unless the user has chosen to customize them
-if not set --query fzf_fish_custom_keybindings
-    # \cf is Ctrl+f
-    bind \cf '__fzf_search_current_dir'
-    bind \cr '__fzf_search_history'
-    bind \cv '__fzf_search_shell_variables'
-    # The following two key binding use Alt as an additional modifier key to avoid conflicts
-    bind \e\cl '__fzf_search_git_log'
-    bind \e\cs '__fzf_search_git_status'
+set -q FZF_TMUX_HEIGHT; or set -U FZF_TMUX_HEIGHT "40%"
+set -q FZF_DEFAULT_OPTS; or set -U FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
+set -q FZF_LEGACY_KEYBINDINGS; or set -U FZF_LEGACY_KEYBINDINGS 1
+set -q FZF_DISABLE_KEYBINDINGS; or set -U FZF_DISABLE_KEYBINDINGS 0
+set -q FZF_PREVIEW_FILE_CMD; or set -U FZF_PREVIEW_FILE_CMD "head -n 10"
+set -q FZF_PREVIEW_DIR_CMD; or set -U FZF_PREVIEW_DIR_CMD "ls"
 
-    # set up the same key bindings for insert mode if using fish_vi_key_bindings
-    if [ $fish_key_bindings = 'fish_vi_key_bindings' ]
-        bind --mode insert \cf '__fzf_search_current_dir'
-        bind --mode insert \cr '__fzf_search_history'
-        bind --mode insert \cv '__fzf_search_shell_variables'
-        bind --mode insert \e\cl '__fzf_search_git_log'
-        bind --mode insert \e\cs '__fzf_search_git_status'
+if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
+    if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
+        bind \ct '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \ec '__fzf_cd'
+        bind \eC '__fzf_cd --hidden'
+        bind \cg '__fzf_open'
+        bind \co '__fzf_open --editor'
+
+        if bind -M insert >/dev/null 2>/dev/null
+            bind -M insert \ct '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \ec '__fzf_cd'
+            bind -M insert \eC '__fzf_cd --hidden'
+            bind -M insert \cg '__fzf_open'
+            bind -M insert \co '__fzf_open --editor'
+        end
+    else
+        bind \co '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \ec '__fzf_cd'
+        bind \eC '__fzf_cd --hidden'
+        bind \eO '__fzf_open'
+        bind \eo '__fzf_open --editor'
+
+        if bind -M insert >/dev/null 2>/dev/null
+            bind -M insert \co '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \ec '__fzf_cd'
+            bind -M insert \eC '__fzf_cd --hidden'
+            bind -M insert \eO '__fzf_open'
+            bind -M insert \eo '__fzf_open --editor'
+        end
+    end
+
+    if not bind --user \t >/dev/null 2>/dev/null
+        if set -q FZF_COMPLETE
+            bind \t '__fzf_complete'
+            if bind -M insert >/dev/null 2>/dev/null
+                bind -M insert \t '__fzf_complete'
+            end
+        end
     end
 end
 
-# If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside of this plugin.
-# See https://github.com/junegunn/fzf#environment-variables
-if not set --query FZF_DEFAULT_OPTS
-    # cycle makes scrolling easier
-    # reverse layout is more familiar as it mimicks the layout of git log, history, and env
-    # border makes clear where the fzf window ends
-    # height 75% allows you to view what you were doing and stay in context of your work
-    # preview-window wrap wraps long lines in the preview window
-    set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height 75% --preview-window=wrap'
+function _fzf_uninstall -e fzf_uninstall
+    bind --user \
+        | string replace --filter --regex -- "bind (.+)( '?__fzf.*)" 'bind -e $1' \
+        | source
+
+    set --names \
+        | string replace --filter --regex '(^FZF)' 'set --erase $1' \
+        | source
+
+    functions --erase _fzf_uninstall
 end
