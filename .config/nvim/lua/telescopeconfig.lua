2c37235e4b5947e724bbe904cc87395671b2cb35
adgai19 <gaitondeaditya@gmail.com>
2021-03-30 17:48:47 +0530

    merging vim configs

--- .config/nvim/lua/telescopeconfig.lua
+++ .config/nvim/lua/telescopeconfig.lua
@@ -1,5 +1,13 @@
 require('telescope').load_extension('fzy_native')
 local M = {}
+require('telescope').setup {
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+    }
+}
 M.search_dotfiles = function() 
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
